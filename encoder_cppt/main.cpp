1b38c6021094d80b8c943dd1f21097aa2eec346b
encoderuz <abduqodirxojiyev@gmail.com>
2024-01-29 19:22:42 +0500

    Initial Commit

---
+++ encoder_cppt/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  encoder_cppt
+//
+//  Created by senior on 29/01/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
